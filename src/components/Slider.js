c3504002683468fefdbd148face0290a3f72c67a
CalvoM <mwadimemakokha@ieee.org>
2019-12-07 01:00:15 +0300

    Begin iteration of Card component

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,8 +2,30 @@ import React from 'react';
 import '../styles/Slider.css';
 import Card from './Card';
 class Slider extends React.Component{
+    constructor(props){
+        super(props);
+        this.renderCard = this.renderCard.bind(this);
+    }
     componentDidMount(){
-        console.log(this.props.cityDetails)
+        console.log(this.props.cityDetails.list)
+    }
+    renderCard(cityDetails){
+        let day_counter = 0;
+        let day_max_temp_list=[];
+        let day_min_temp_list=[];
+        for(let hour_details of cityDetails){
+            if(day_counter%8===0){
+                console.log(day_max_temp_list)
+                day_min_temp_list.length=0;
+                day_max_temp_list.length=0;
+                day_counter = 0;
+                return <Card />
+            }
+            day_max_temp_list.push(+hour_details.main.temp_max);
+            day_min_temp_list.push(+hour_details.main.temp_min)
+            day_counter +=1;
+        } 
+        
     }
     render(){
         return(
@@ -11,10 +33,8 @@ class Slider extends React.Component{
     <div className="cityTag">{this.props.cityName},
     <span className="countryTag">{this.props.cityDetails.city.country}</span>
     </div>
-            {(this.props.cityDetails.list.length/8)
-            }
-                <Card />               
-            </div>
+        {this.renderCard(this.props.cityDetails.list)}           
+    </div>
         );
     }
 }
